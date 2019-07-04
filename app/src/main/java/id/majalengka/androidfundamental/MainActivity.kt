b19ce35ced61db2b0c50a50902413d49f1f4dec3
Ihwan <ihwan@indosystem.com>
2019-07-04 20:36:54 +0700

    migrate to androidX & modularization dependency

--- app/src/main/java/id/majalengka/androidfundamental/MainActivity.kt
+++ app/src/main/java/id/majalengka/androidfundamental/MainActivity.kt
@@ -1,6 +1,6 @@
 package id.majalengka.androidfundamental
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
