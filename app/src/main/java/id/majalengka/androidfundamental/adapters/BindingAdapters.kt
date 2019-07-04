cd0d5a764462dc29cad13ac832badb98ecf78c70
Ihwan <ihwan@indosystem.com>
2019-07-04 21:50:44 +0700

    add BindingAdapters & add more dependency

---
+++ app/src/main/java/id/majalengka/androidfundamental/adapters/BindingAdapters.kt
@@ -0,0 +1,41 @@
+package id.majalengka.androidfundamental.adapters
+
+import android.text.method.LinkMovementMethod
+import android.view.View
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
+import androidx.core.text.HtmlCompat.FROM_HTML_MODE_COMPACT
+import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
+
+@BindingAdapter("isGone")
+fun bindIsGone(view: View, isGone: Boolean) {
+    view.visibility = if (isGone) {
+        View.GONE
+    } else {
+        View.VISIBLE
+    }
+}
+
+@BindingAdapter("imageFromUrl")
+fun bindImageFromUrl(view: ImageView, imageUrl: String?) {
+    if (!imageUrl.isNullOrEmpty()) {
+        Glide.with(view.context)
+                .load(imageUrl)
+                .transition(DrawableTransitionOptions.withCrossFade())
+                .into(view)
+    }
+}
+
+
+@BindingAdapter("renderHtml")
+fun bindRenderHtml(view: TextView, description: String?) {
+    if (description != null) {
+        view.text = HtmlCompat.fromHtml(description, FROM_HTML_MODE_COMPACT)
+        view.movementMethod = LinkMovementMethod.getInstance()
+    } else {
+        view.text = ""
+    }
+}
