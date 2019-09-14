ad690d9580780b12fd68490f39aa1cae8316fba6
Ihwan <ihwan@indosystem.com>
2019-09-14 16:52:17 +0700

    call data from web service

--- app/src/main/java/id/majalengka/androidfundamental/network/Webservice.kt
+++ app/src/main/java/id/majalengka/androidfundamental/network/Webservice.kt
@@ -1,12 +1,11 @@
 package id.majalengka.androidfundamental.network
 
 import id.majalengka.androidfundamental.model.ResultResponse
-import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface Webservice {
 
     @GET("{location}.json")
-    suspend fun getPrayerTime(@Path("location") location: String ): Response<ResultResponse>
+    suspend fun getPrayerTime(@Path("location") location: String ): ResultResponse
 }
--- app/src/main/java/id/majalengka/androidfundamental/ui/PrayerTimeFragment.kt
+++ app/src/main/java/id/majalengka/androidfundamental/ui/PrayerTimeFragment.kt
@@ -1,20 +1,22 @@
 package id.majalengka.androidfundamental.ui
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import dagger.android.support.DaggerFragment
 import id.majalengka.androidfundamental.viewmodel.MainViewModel
 import id.majalengka.androidfundamental.databinding.FragmentPrayerTimeBinding
 import id.majalengka.androidfundamental.adapters.PrayerTimeAdapter
-import id.majalengka.androidfundamental.utils.Result
+
 import javax.inject.Inject
 
-class PrayerTimeFragment : Fragment() {
+class PrayerTimeFragment : DaggerFragment() {
 
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private val viewModel: MainViewModel by viewModels { viewModelFactory }
@@ -22,37 +24,39 @@ class PrayerTimeFragment : Fragment() {
         PrayerTimeAdapter()
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View? {
+    lateinit var binding: FragmentPrayerTimeBinding
 
-        val binding = FragmentPrayerTimeBinding.inflate(inflater, container, false)
-                .apply {
-                    recyclerView.adapter = adapter
-                }
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentPrayerTimeBinding.inflate(inflater, container, false)
 
-
-
-        subscribeUi(binding, adapter)
-
-        setHasOptionsMenu(true)
         return binding.root
     }
 
-    private fun subscribeUi(binding: FragmentPrayerTimeBinding, adapter: PrayerTimeAdapter) {
-        viewModel.data.observe(viewLifecycleOwner, Observer { result ->
-            when (result.status) {
-                Result.Status.SUCCESS -> {
-                    //binding.progressBar.hide()
-                    result.data?.let { adapter.submitList(it.items) }
-                }
-                Result.Status.LOADING -> {
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        subscribeUi()
+    }
 
-                }//binding.progressBar.show()
-                Result.Status.ERROR -> {
-                    ///binding.progressBar.hide()
-                    //Snackbar.make(binding.root, result.message!!, Snackbar.LENGTH_LONG).show()
-                }
-            }
+    private fun subscribeUi() {
+        viewModel.data.observe(viewLifecycleOwner, Observer { result ->
+            Toast.makeText(activity, result.toString(), Toast.LENGTH_SHORT).show()
+//            when (result.status) {
+//                Result.Status.SUCCESS -> {
+//                    //binding.progressBar.hide()
+//                    result.data?.let { adapter.submitList(it.items) }
+//                }
+//                Result.Status.LOADING -> {
+//
+//                }//binding.progressBar.show()
+//                Result.Status.ERROR -> {
+//                    ///binding.progressBar.hide()
+//                    //Snackbar.make(binding.root, result.message!!, Snackbar.LENGTH_LONG).show()
+//                }
+//            }
         })
     }
 
